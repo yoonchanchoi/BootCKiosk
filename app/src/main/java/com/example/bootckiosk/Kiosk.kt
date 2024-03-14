42a4c06c18705d5fd07475261c519db1142594de
Choi YoonChan <choyc9239@gmail.com>
2024-03-14 13:01:04 +0900

    주석 수정

--- app/src/main/java/com/example/bootckiosk/Kiosk.kt
+++ app/src/main/java/com/example/bootckiosk/Kiosk.kt
@@ -32,7 +32,6 @@ class Kiosk {
                         all.filter { it.category == "Burger" } as ArrayList<ShakeshackMenu>
                     categoryMenu(title, categorys)
                     categorys.clear()
-
                 }
 
                 "2" -> {
@@ -63,7 +62,6 @@ class Kiosk {
                     println("다시 입력해주세용")
                     println()
                 }
-
             }
         }
     }
@@ -72,7 +70,6 @@ class Kiosk {
 
         var input = ""
         var select = -1
-//        var flag =-1
         while (true) {
 
             println(title)
@@ -91,11 +88,9 @@ class Kiosk {
                 continue
             }
             if (select == 0) {
-//                flag = 1
                 return
             } else if (0 < select && select <= categoryMenus.size) {
                 printSelecMenu(select, categoryMenus)
-//                flag = 1
             } else {
                 println("다시 입력해주세용")
                 println()
@@ -114,9 +109,6 @@ class Kiosk {
         }
     }
 
-
-
-
     private fun initData(): ArrayList<ShakeshackMenu>{
         val all = arrayListOf<ShakeshackMenu>()
 
@@ -134,6 +126,7 @@ class Kiosk {
             Burger(4,"쉑 스택", "포토벨로 버섯 패티, 비프 패티와 함께 토마토, 양상추, 쉑소스가 토핑된 치즈버거",14800,"Burger"),
             Burger(5,"햄버거", "포테이토 번과 비프 패티를 기본으로 신선한 양상추, 토마토, 피클, 양파 토핑을 취향에 따라 선택할 수 있는 버거",6800, "Burger" )
         )
+
         val drinks = arrayListOf<ShakeshackMenu>(
             Drink(1,"레몬에이드", "매장에서 직접 만드는 상큼한 레몬에이드", 4300, "Drink" ),
             Drink(2,"아이스 티", "직접 유기농 홍차를 우려낸 아이스 티", 3500, "Drink" ),
@@ -141,16 +134,19 @@ class Kiosk {
             Drink(4,"탄산음료", "코카콜라, 코카콜라 제로, 스프라이트, 환타 오렌지, 환타 그레이프, 환타 파인애플",2900,"Drink" ),
             Drink(5,"루트 비어", "청량감 있는 독특한 미국식 무알콜 탄산음료",4800,"Drink"),
         )
+
         val forzenCustards = arrayListOf<ShakeshackMenu>(
             ForzenCustard(1,"쉐이크", "쫀득하고 진한 커스터드가 들어간 클래식 쉐이크(바닐라/초콜릿/스트로베리/블랙 & 화이트/솔티드 카라멜/피넛 버터/커피)",6500,"ForzenCustard"),
             ForzenCustard(2,"플로트", "부드러운 바닐라 커스터드와 톡톡 터지는 탄산이 만나 탄생한 색다른 음료(루트 비어/퍼플 카우/크림시클)",6500,"ForzenCustard"),
             ForzenCustard(3,"컵 & 콘", "매일 점포에서 신선하게 제조하는 쫀득하고 진한 아이스크림 (바닐라/초콜릿)",5400,"ForzenCustard"),
         )
+
         val chickens = arrayListOf<ShakeshackMenu>(
             Chicken(1,"핫 치킨", "바삭하고 두툼한 치킨 통살과 스파이시 슬로, 핫 스파이시 시즈닝이 토핑된 치킨 버거 (닭가슴살/닭다리살 선택 가능)",9500,"Chicken"),
             Chicken(2,"치킨쉑", "바삭하고 두툼한 치킨 통살과 양상추, 피클, 허브 마요 소스가 토핑된 치킨 버거 (닭가슴살/닭다리살 선택 가능)",8000, "Chicken" ),
             Chicken(3,"치킨 바이트", "한 입에 먹기 좋은 바삭한 치킨 바이트와 허니 머스터드 또는 BBQ 소스를 선택하여 함께 즐기는 메뉴",5900 ,"Chicken"),
         )
+
         all.addAll(menu)
         all.addAll(burgers)
         all.addAll(drinks)
@@ -158,7 +154,6 @@ class Kiosk {
         all.addAll(chickens)
         return all
     }
-
 }
 
 
